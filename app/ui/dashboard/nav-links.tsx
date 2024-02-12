850ca597d39430f6afa65d99ddc79eaa4e673675
SAMSUNGPROJECT\ws <ws@cin.ufpe.br>
2024-02-11 22:30:44 -0300

    :tada: update all project files

---
+++ app/ui/dashboard/nav-links.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import styles from '@/app/ui/dashboard/nav-links.module.css';
+import {
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={`${styles.link} ${
+              pathname == link.href ? styles.active : ''
+            }`}
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
